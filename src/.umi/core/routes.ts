a0a3a9d84b995186c75e9ea4d3995c5ed228ac58
wangshun <shun.wang@77hub.com>
2022-03-31 14:06:31 +0800

    dev

--- src/.umi/core/routes.ts
+++ src/.umi/core/routes.ts
@@ -52,6 +52,115 @@ export function getRoutes() {
     "wrappers": [require('/Users/wangshun/Desktop/conclusion/node_modules/@umijs/preset-dumi/lib/theme/layout').default, require('/Users/wangshun/Desktop/conclusion/node_modules/dumi-theme-default/src/layout.tsx').default],
     "routes": [
       {
+        "path": "/go/container",
+        "component": require('/Users/wangshun/Desktop/conclusion/src/GO/container.md').default,
+        "exact": true,
+        "meta": {
+          "filePath": "src/GO/container.md",
+          "updatedTime": 1647588070676,
+          "slugs": [
+            {
+              "depth": 3,
+              "value": "Container",
+              "heading": "container"
+            },
+            {
+              "depth": 4,
+              "value": "Array",
+              "heading": "array"
+            },
+            {
+              "depth": 4,
+              "value": "Slice",
+              "heading": "slice"
+            },
+            {
+              "depth": 5,
+              "value": "reslice",
+              "heading": "reslice"
+            },
+            {
+              "depth": 5,
+              "value": "slice的扩展",
+              "heading": "slice的扩展"
+            },
+            {
+              "depth": 5,
+              "value": "slice的实现",
+              "heading": "slice的实现"
+            }
+          ],
+          "title": "Container",
+          "group": {
+            "path": "/go",
+            "title": "Go"
+          }
+        },
+        "title": "Container"
+      },
+      {
+        "path": "/go",
+        "component": require('/Users/wangshun/Desktop/conclusion/src/GO/index.md').default,
+        "exact": true,
+        "meta": {
+          "filePath": "src/GO/index.md",
+          "updatedTime": 1647506098495,
+          "slugs": [
+            {
+              "depth": 3,
+              "value": "Basic grammar",
+              "heading": "basic-grammar"
+            },
+            {
+              "depth": 4,
+              "value": "变量定义",
+              "heading": "变量定义"
+            },
+            {
+              "depth": 5,
+              "value": "使用var关键字",
+              "heading": "使用var关键字"
+            },
+            {
+              "depth": 4,
+              "value": "常量定义",
+              "heading": "常量定义"
+            },
+            {
+              "depth": 5,
+              "value": "使用const关键字",
+              "heading": "使用const关键字"
+            },
+            {
+              "depth": 4,
+              "value": "条件控制",
+              "heading": "条件控制"
+            },
+            {
+              "depth": 4,
+              "value": "循环",
+              "heading": "循环"
+            },
+            {
+              "depth": 4,
+              "value": "函数",
+              "heading": "函数"
+            },
+            {
+              "depth": 4,
+              "value": "指针",
+              "heading": "指针"
+            }
+          ],
+          "title": "Basic grammar",
+          "group": {
+            "path": "/go",
+            "title": "Go"
+          }
+        },
+        "title": "Basic grammar"
+      },
+      {
         "path": "/react/element",
         "component": require('/Users/wangshun/Desktop/conclusion/src/React/element.md').default,
         "exact": true,
@@ -131,7 +240,7 @@ export function getRoutes() {
         "exact": true,
         "meta": {
           "filePath": "src/babel/index.md",
-          "updatedTime": 1622721509000,
+          "updatedTime": 1628574786000,
           "slugs": [
             {
               "depth": 2,
@@ -163,12 +272,84 @@ export function getRoutes() {
         "title": "Babel"
       },
       {
+        "path": "/browser/macro-browser",
+        "component": require('/Users/wangshun/Desktop/conclusion/src/browser/macroBrowser.md').default,
+        "exact": true,
+        "meta": {
+          "filePath": "src/browser/macroBrowser.md",
+          "updatedTime": 1647487537000,
+          "slugs": [
+            {
+              "depth": 3,
+              "value": "浏览器 \b",
+              "heading": "浏览器-\b"
+            },
+            {
+              "depth": 4,
+              "value": "浏览器多进程架构",
+              "heading": "浏览器多进程架构"
+            },
+            {
+              "depth": 4,
+              "value": "TCP 协议",
+              "heading": "tcp-协议"
+            },
+            {
+              "depth": 5,
+              "value": "你怎么理解 HTTP 和 TCP 的关系？",
+              "heading": "你怎么理解-http-和-tcp-的关系？"
+            },
+            {
+              "depth": 4,
+              "value": "HTTP 请求流程",
+              "heading": "http-请求流程"
+            },
+            {
+              "depth": 4,
+              "value": "URL 输入到页面展示经历了什么？",
+              "heading": "url-输入到页面展示经历了什么？"
+            },
+            {
+              "depth": 5,
+              "value": "用户输入",
+              "heading": "用户输入"
+            },
+            {
+              "depth": 5,
+              "value": "URL 请求",
+              "heading": "url-请求"
+            },
+            {
+              "depth": 5,
+              "value": "准备渲染进程",
+              "heading": "准备渲染进程"
+            },
+            {
+              "depth": 5,
+              "value": "提交文档",
+              "heading": "提交文档"
+            },
+            {
+              "depth": 5,
+              "value": "渲染",
+              "heading": "渲染"
+            }
+          ],
+          "title": "浏览器 \b",
+          "group": {
+            "path": "/browser",
+            "title": "Browser"
+          }
+        },
+        "title": "浏览器 \b"
+      },
+      {
         "path": "/leetcode",
         "component": require('/Users/wangshun/Desktop/conclusion/src/leetcode/index.md').default,
         "exact": true,
         "meta": {
           "filePath": "src/leetcode/index.md",
-          "updatedTime": 1618151108512,
+          "updatedTime": 1628574786000,
           "slugs": [
             {
               "depth": 2,
@@ -215,7 +396,7 @@ export function getRoutes() {
         "exact": true,
         "meta": {
           "filePath": "src/leetcode/link.md",
-          "updatedTime": 1618151108544,
+          "updatedTime": 1628574786000,
           "slugs": [
             {
               "depth": 2,
@@ -252,7 +433,7 @@ export function getRoutes() {
         "exact": true,
         "meta": {
           "filePath": "src/leetcode/stack.md",
-          "updatedTime": 1618151108558,
+          "updatedTime": 1628574786000,
           "slugs": [
             {
               "depth": 2,
@@ -326,78 +507,6 @@ export function getRoutes() {
         "title": "根据 Promise/A+ 规范实现 Promise"
       },
       {
-        "path": "/浏览器方面/宏观看浏览器",
-        "component": require('/Users/wangshun/Desktop/conclusion/src/浏览器方面/宏观看浏览器.md').default,
-        "exact": true,
-        "meta": {
-          "filePath": "src/浏览器方面/宏观看浏览器.md",
-          "updatedTime": 1628154593138,
-          "slugs": [
-            {
-              "depth": 3,
-              "value": "浏览器\b",
-              "heading": "浏览器\b"
-            },
-            {
-              "depth": 4,
-              "value": "浏览器多进程架构",
-              "heading": "浏览器多进程架构"
-            },
-            {
-              "depth": 4,
-              "value": "TCP协议",
-              "heading": "tcp协议"
-            },
-            {
-              "depth": 5,
-              "value": "你怎么理解 HTTP 和 TCP 的关系？",
-              "heading": "你怎么理解-http-和-tcp-的关系？"
-            },
-            {
-              "depth": 4,
-              "value": "HTTP请求流程",
-              "heading": "http请求流程"
-            },
-            {
-              "depth": 4,
-              "value": "URL输入到页面展示经历了什么？",
-              "heading": "url输入到页面展示经历了什么？"
-            },
-            {
-              "depth": 5,
-              "value": "用户输入",
-              "heading": "用户输入"
-            },
-            {
-              "depth": 5,
-              "value": "URL 请求",
-              "heading": "url-请求"
-            },
-            {
-              "depth": 5,
-              "value": "准备渲染进程",
-              "heading": "准备渲染进程"
-            },
-            {
-              "depth": 5,
-              "value": "提交文档",
-              "heading": "提交文档"
-            },
-            {
-              "depth": 5,
-              "value": "渲染",
-              "heading": "渲染"
-            }
-          ],
-          "title": "浏览器\b",
-          "group": {
-            "path": "/浏览器方面",
-            "title": "浏览器方面"
-          }
-        },
-        "title": "浏览器\b"
-      },
-      {
         "path": "/",
         "component": require('/Users/wangshun/Desktop/conclusion/docs/index.md').default,
         "exact": true,
@@ -416,16 +525,16 @@ export function getRoutes() {
         "title": "Hello XTT!"
       },
       {
+        "path": "/browser",
+        "meta": {},
+        "exact": true,
+        "redirect": "/browser/macro-browser"
+      },
+      {
         "path": "/promise",
         "meta": {},
         "exact": true,
         "redirect": "/promise/impl-promise"
-      },
-      {
-        "path": "/浏览器方面",
-        "meta": {},
-        "exact": true,
-        "redirect": "/浏览器方面/宏观看浏览器"
       }
     ],
     "title": "XTT",
